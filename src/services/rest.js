9629a957c80eb1e39ea75a6228d2a33e37581b0f
Jesus Miguel Quinto <jesuskinto@gmail.com>
2020-04-11 12:53:27 -0400

    envs

--- src/services/rest.js
+++ src/services/rest.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-const res = axios.create({
-  baseURL: "http://localhost:3000/api/",
-});
-
+const res = axios.create({ baseURL: process.env.HOST });
 res.interceptors.request.use(function (config) {
   const token = sessionStorage.getItem("token");
   config.headers["access-token"] = token;
