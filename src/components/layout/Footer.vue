d418cbd882483ea41c3ee480fe7d8c19a954967b
Jesus Miguel Quinto <jesuskinto@gmail.com>
2020-04-11 14:31:42 -0400

    config in footer

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -4,5 +4,39 @@
       Hecho con ❤ por 💻
       <a target="_blank" href="http://portafolio.jesuskinto.now.sh/">@jesuskinto</a>
     </div>
+    <div class="data">
+      <small>
+        <strong>VERSION:</strong>
+        1.0.0
+      </small>
+      <br />
+      <small>
+        <strong>API:</strong>
+        {{ API }}
+      </small>
+    </div>
   </footer>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      API: process.env.VUE_APP_API_URL
+    };
+  }
+};
+</script>
+
+<style scoped>
+.footer {
+  position: relative;
+}
+.data {
+  color: gray;
+  position: absolute;
+  right: 10px;
+  bottom: 10px;
+  text-align: right;
+}
+</style>
